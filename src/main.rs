43a6be9cc1031e7f53b2f6951bb815d195356190
rickypang0219 <rickypang0219@gmail.com>
2024-06-24 23:35:22 +0800

    add fetch function to fetch binance data and serde to json formate

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 use reqwest;
 use reqwest::Error;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct FuturePrice {
+    timestamp: i64,
+    open: String,
+    high: String,
+    low: String,
+    close: String,
+    volume: String,
+    close_time: i64,
+    quote_asset_volume: String,
+    number_of_trades: i64,
+    taker_buy_volume: String,
+    taker_buy_quote_asset_volume: String,
+    ignore: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -8,13 +25,14 @@ async fn main() -> Result<(), Error> {
         ("pair", "BTCUSDT"),
         ("contractType", "PERPETUAL"),
         ("interval", "1m"),
+        ("limit", "1"),
     ];
-    let response = client
+    let response: Vec<FuturePrice> = client
         .get("https://fapi.binance.com/fapi/v1/continuousKlines")
         .query(&params)
         .send()
         .await?
-        .text()
+        .json()
         .await?;
 
     println!("Response: {:#?}", response);
