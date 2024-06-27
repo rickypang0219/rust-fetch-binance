11db2e648417bc3d95f82e6a9c2e73f29633e0b0
rickypang0219 <rickypang0219@gmail.com>
2024-06-28 00:02:54 +0800

    add function to convert Vec<FuturePrice> to DataFrame

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use polars::prelude::*;
 use reqwest;
 use reqwest::Error;
 use serde::{Deserialize, Serialize};
@@ -18,6 +19,67 @@ struct FuturePrice {
     ignore: String,
 }
 
+fn convert_futures_data_dataframe(
+    future_data: Vec<FuturePrice>,
+) -> Result<polars::prelude::DataFrame, PolarsError> {
+    let timestamp: Vec<i64> = future_data.iter().map(|f| f.timestamp.clone()).collect();
+    let open: Vec<f64> = future_data
+        .iter()
+        .map(|f| f.open.clone().parse::<f64>().unwrap())
+        .collect();
+    let high: Vec<f64> = future_data
+        .iter()
+        .map(|f| f.high.clone().parse::<f64>().unwrap())
+        .collect();
+    let low: Vec<f64> = future_data
+        .iter()
+        .map(|f| f.low.clone().parse::<f64>().unwrap())
+        .collect();
+    let close: Vec<f64> = future_data
+        .iter()
+        .map(|f| f.close.clone().parse::<f64>().unwrap())
+        .collect();
+    let volume: Vec<f64> = future_data
+        .iter()
+        .map(|f| f.volume.clone().parse::<f64>().unwrap())
+        .collect();
+    let close_time: Vec<i64> = future_data.iter().map(|f| f.close_time.clone()).collect();
+    let quote_asset_volume: Vec<f64> = future_data
+        .iter()
+        .map(|f| f.volume.clone().parse::<f64>().unwrap())
+        .collect();
+    let number_of_trades: Vec<i64> = future_data
+        .iter()
+        .map(|f| f.number_of_trades.clone())
+        .collect();
+    let taker_buy_volume: Vec<f64> = future_data
+        .iter()
+        .map(|f| f.taker_buy_volume.clone().parse::<f64>().unwrap())
+        .collect();
+    let taker_buy_quote_asset_volume: Vec<f64> = future_data
+        .iter()
+        .map(|f| {
+            f.taker_buy_quote_asset_volume
+                .clone()
+                .parse::<f64>()
+                .unwrap()
+        })
+        .collect();
+    df!(
+        "timestamp" => &timestamp,
+        "open" => &open,
+        "high" => &high,
+        "low" => &low,
+        "close" => &close,
+        "volume" => &volume,
+        "close_time" =>close_time,
+        "quote_asset_volume" => &quote_asset_volume,
+        "number_of_trade" => &number_of_trades,
+        "taker_buy_volume" => &taker_buy_volume,
+        "taker_buy_quote_asset_volume" => &taker_buy_quote_asset_volume,
+    )
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let client = reqwest::Client::new();
@@ -36,13 +98,9 @@ async fn main() -> Result<(), Error> {
         .await?
         .json()
         .await?;
-    // println!("Response: {:#?}", response);
 
-    let close_prices: Vec<f64> = response
-        .iter()
-        .map(|fp| fp.close.clone().parse::<f64>().unwrap())
-        .collect();
-    println!("Close Prices: {:?}", close_prices);
+    let futures_df = convert_futures_data_dataframe(response);
+    println!("{:#?}", futures_df);
 
     Ok(())
 }
